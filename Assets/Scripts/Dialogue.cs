053758dfbae65e2e7eace110eb6d6ec8bd3f6a69
Matt Conway <matthewjosephconway@gmail.com>
2022-02-16 10:18:16 -0800

    W07 Dialogue/UI Updates

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -8,6 +8,11 @@ public class Dialogue : MonoBehaviour
     public GameObject Balloon;
     public string Text;
 
+    public AudioSource audio1;
+    public AudioClip[] clips;
+
+    public GameObject Appear;
+
     private GameObject _curBalloon;
 
     private void OnTriggerEnter(Collider col)
@@ -20,12 +25,35 @@ public class Dialogue : MonoBehaviour
         _curBalloon.GetComponent<RectTransform>().position = new Vector2(Screen.width / 2.0f, 15); //position text balloon
         _curBalloon.GetComponent<Text>().text = Text; //update text in balloon
         _curBalloon.transform.parent = canvas.transform; //add balloon under Canvas
+
+        //font size
+        int screenHeight = Screen.height;
+        float fontSize = screenHeight / 10.0f; Debug.Log((int)fontSize);
+        _curBalloon.GetComponent<Text>().fontSize = (int)fontSize; //update text in balloon
+        Vector2 rectSize = _curBalloon.GetComponent<RectTransform>().sizeDelta;
+        rectSize.y = (int)(fontSize / 0.825);
+        _curBalloon.GetComponent<RectTransform>().sizeDelta = rectSize;
+
+        //object show
+        if (Appear != null) { Appear.SetActive(true); }
+
+        //audio
+        if (audio1 == null) { return; }
+        if (clips.Length == 0) { return; }
+        int i = Random.Range(0, clips.Length); //random number within clips length
+        audio1.clip = clips[c];
+        audio1.Play();
+        c++;
+        if (c >= clips.Length) { c = 0; }
     }
+
+    private int c = 0;
 
     private void OnTriggerExit(Collider col)
     {
         if (col.tag != "Player") { return; }
         if (_curBalloon == null) { return; }
+        if (Appear != null) { Appear.SetActive(false); }
         Destroy(_curBalloon);
     }
 }
